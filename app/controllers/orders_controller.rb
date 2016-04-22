617fde8da09d32341b135f9e89fd44f1324123ed
filipe_torrado <filipe.torrado89@gmail.com>
2016-04-22 16:18:12 +0100

    version to deliver FeedbackFruits

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,27 @@
 class OrdersController < ApplicationController
   def list_names
-    @orders = Order.where(name: params[:query])
+    @query = /.*#{Regexp.escape(params[:query])}.*/i
+    stages = [
+        {"$match"=> {"name" => {"$regex" => @query}}},
+        {"$group"=> { "_id" => {"result" => "$name"}}}
+    ]
+    @orders = Order.collection.aggregate(stages)
     @list = Array.new
     @orders.each do |order|
-      @list.push order.name
+      @list.push order['_id']['result']
     end
     render 'list', layout: false
   end
   def list_dishes
-    @orders = Order.where(dish: params[:query])
+    @query = /.*#{Regexp.escape(params[:query])}.*/i
+    stages = [
+        {"$match"=> {"dish" => {"$regex" => @query}}},
+        {"$group"=> { "_id" => {"result" => "$dish"}}}
+    ]
+    @orders = Order.collection.aggregate(stages)
     @list = Array.new
     @orders.each do |order|
-      @list.push order.dish
+      @list.push order['_id']['result']
     end
     render 'list', layout: false
   end
